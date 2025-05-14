f4f7550a23f04914e44cd21bdafe27f6eea021d2
João Vitor Campos <vitorov009@gmail.com>
2025-05-14 09:27:43 -0300

    vite @

--- Atividade03-Musica/resources/views/components/layout.blade.php
+++ Atividade03-Musica/resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Spotify</title>
+    <title>Spotify @</title>
     @vite(['resources/css/app.css'])
 
 </head>
