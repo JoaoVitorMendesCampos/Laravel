2ae31e84b56901bc70996b7e965308e71011f6b8
João Vitor Campos <vitorov009@gmail.com>
2025-05-14 09:44:02 -0300

    atualização login

--- pratica-laravel/routes/web.php
+++ pratica-laravel/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 use App\Http\Controllers\LivroController;
-
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,14 +9,26 @@ Route::get('/', function () {
 
 });
 
-Route::get('/livros', [LivroController::class, 'exibirLivros']);
 
-Route::get('/livros/criar', [LivroController::class, 'criarLivro']);
+Route::get('/livros', [LivroController::class, 'exibirLivros'])->middleware('auth');
 
-Route::post('/livros/armazenar', [LivroController::class, 'armazenarLivro']);
+Route::get('/livros/criar', [LivroController::class, 'criarLivro'])->middleware('auth');
 
-Route::post('/livros/editar', [LivroController::class, 'editarLivro']);
+Route::post('/livros/armazenar', [LivroController::class, 'armazenarLivro'])->middleware('auth');
 
-Route::post('/livros/atualizar', [LivroController::class, 'atualizarLivro']);
+Route::post('/livros/editar', [LivroController::class, 'editarLivro'])->middleware('auth');
 
-Route::post('/livros/deletar', [LivroController::class, 'deletarLivro']);
+Route::post('/livros/atualizar', [LivroController::class, 'atualizarLivro'])->middleware('auth');
+
+Route::post('/livros/deletar', [LivroController::class, 'deletarLivro'])->middleware('auth');
+
+
+
+Route::get('/login', [LoginController::class, 'digitarLogin']);
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/logout', [LoginController::class, 'logout']);
+
+
+
