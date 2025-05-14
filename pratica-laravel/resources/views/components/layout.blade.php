220ea6bfd5f2aa27e23cd35be232e456815462d8
João Vitor Campos <vitorov009@gmail.com>
2025-05-14 10:44:04 -0300

    ll

--- pratica-laravel/resources/views/components/layout.blade.php
+++ pratica-laravel/resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Prática 10Laravel</title>
+    <title>Prática Laravel</title>
 
     @vite(['resources/css/app.css'])
 
